b36d4993979c5d19c2461df70f33e8bea4f7926b
ZacharyFolk <zacharyfolk@gmail.com>
2021-07-14 14:59:19 -0700

    Simple middleware to create tokenized requests

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,23 @@
+// we create own middleware than using bloated passport, that is good if want fb login, etc..
 const jwt = require('jsonwebtoken');
+const config = require('config');
+
+// middleware function has access to request, response objects, also a callback to run to next piece of middleware
+module.exports = function (req, res, next) {
+  // Get token from header
+  const token = req.header('x-auth-token');
+  // Check for token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' }); // 401 = not authorized
+  }
+
+  // Verify token
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/Users');
 
 // @route       GET api/auth
 // @desc        Auth route
 // @access      Public (no need for token, will create auth middleware)
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password'); // from middleware auth.js we have decoded.user - password
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
