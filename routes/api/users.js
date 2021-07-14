7a4350527814d557a5baecd95164c7103cfada88
ZacharyFolk <zacharyfolk@gmail.com>
2021-07-14 10:22:42 -0700

    Add request validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,31 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator/check");
+// Validator docs : https://express-validator.github.io/docs/
 
 // @route       GET api/users
-// @desc        Test route
+// @desc        Register user
 // @access      Public (no need for token, will create auth middleware)
-router.get("/", (req, res) => res.send("User route"));
+
+// router.get("/", (req, res) => res.send("User route"));
+
+router.post(
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Enter a password with 6 or more chars").isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    console.log(req.body);
+    res.send("User route");
+  }
+);
 
 module.exports = router;
