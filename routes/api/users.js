ba310bb04cf7e2aa2944722177d015e8d53ccef5
ZacharyFolk <zacharyfolk@gmail.com>
2021-07-14 13:39:30 -0700

    Validation and tokenization

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { check, validationResult } = require("express-validator");
+
+const User = require("../../models/Users");
 // Validator docs : https://express-validator.github.io/docs/
 
 // @route       GET api/users
@@ -18,13 +24,73 @@ router.post(
       min: 6,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    console.log(req.body);
-    res.send("User route");
+
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user exists (avoid multiple emails)
+      let user = await User.findOne({ email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: "User already exists" }] }); // match same type of error as validation errors
+      }
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm", // a default avatar
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      // Encrypt the password
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save(); // anything that returns a promise should use await, cleaner than using a lot of .then()
+
+      // res.send("User registered");
+
+      // Return jsonwebtoken so on frontend user is logged in immediately
+      // https://github.com/auth0/node-jsonwebtoken
+      // web token allows to access protected areas after register
+
+      const payload = {
+        user: {
+          // get id from earlier promise
+          id: user.id, // mongoose abstraction removes need to use the _ that is in mdb
+        },
+      };
+
+      jwt.sign(
+        // assign the token
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 3600000 }, // optional, change to 3600/1 hr
+        (err, token) => {
+          // callback arrow function
+          if (err) throw err;
+          res.json({ token }); // could send user.id but just using token
+          // use site https://jwt.io  to decode encryption of token
+        }
+      ); // put the secret in config/default.json
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
