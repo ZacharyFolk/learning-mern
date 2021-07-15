d41baa63411da4c4097da0461ce7d50da7b04ca6
ZacharyFolk <zacharyfolk@gmail.com>
2021-07-15 14:10:10 -0700

    Route to get current user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth'); // now can add this as a second parameter
+const Profile = require('../../models/Profile');
+const User = require('../../models/Users');
 
-// @route       GET api/profile
-// @desc        Test route
-// @access      Public (no need for token, will create auth middleware)
-router.get('/', (req, res) => res.send('Profile route'));
+// @route       GET api/profile/me
+// @desc        Get current user profile
+// @access      Private (requires token)
+router.get('/me', auth, async (req, res) => {
+  // async await because mongoose returns promise
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      // Cool! Use populate to get more info from user model
+      'user',
+      ['name', 'avatar']
+    );
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
