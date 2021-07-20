96e52dc2c6cadcb197c802202dc3bb767e26f164
ZacharyFolk <zacharyfolk@gmail.com>
2021-07-20 09:30:30 -0700

    Route to delete an experience

--- routes/test/derp.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// @route       GET api/auth
-// @desc        Auth route
-// @access      Public (no need for token, will create auth middleware)
-router.get('/', (req, res) => res.send('Derrrrp route'));
-
-module.exports = router;
